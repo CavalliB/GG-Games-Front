6a24d0c4741c852f9e031dcb3e121502da232919
CavalliB <119020580+CavalliB@users.noreply.github.com>
2025-10-28 22:25:59 +0000

    fix nav v2, nueva route perfil

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Vibora from "./Vibora.jsx";
 import Packman from "./Packman.jsx";
 import CaraoCruz from "./CaraoCruz.jsx";
 import Nav from "./Nav.jsx";
+import UsuarioLogin from "./UsuarioLogin.jsx";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
         <Nav />
+
+      <div className="app-container">
         <main>
           <Routes>
             <Route path="/" element={<Principal />} />
             <Route path="/vibora" element={<Vibora />} />
             <Route path="/packman" element={<Packman />} />
             <Route path="/caraocruz" element={<CaraoCruz />} />
+            <Route path="/perfil" element={<UsuarioLogin />} />
             {/* Agregar mas juegos con esto */}
           </Routes>
         </main>
