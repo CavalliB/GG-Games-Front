89999505b462517aa607922972ee99ef6429cf42
CavalliB <119020580+CavalliB@users.noreply.github.com>
2025-10-28 23:14:17 +0000

    fix nav v3 + font

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -10,10 +10,10 @@ function Nav({ setSeccion }) {
     return (
         <div className="containerNav">
          
-            <button className="home" onClick={() => navigate("/")}  element={<Principal />}>Home</button>
+            <button className="home" onClick={() => navigate("/")}  element={<Principal />}>GG</button>
 
-{            <button className='usericon' onClick={() => navigate("/Perfil")} ><img className='usericon' src="https://imgs.search.brave.com/uRLjIz0r9LwrGq9jagcfeSqoD188L_55nkk0IhaFSrw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTk5/NDQ1MzIyL3Bob3Rv/L25hbmR1LXJoZWEt/YW1lcmljYW5hLWdy/ZWF0ZXItcmhlYS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/NGc2MDRpUFBPbDBH/LXlzalBKalVnTmdx/bnFvMGl4SFFGWGhK/dnRhMWRFWT0" alt="Icono" /> </button>
-}        </div>
+            <button className='home' onClick={() => navigate("/Perfil")} ><img className='usericon' src="https://imgs.search.brave.com/uRLjIz0r9LwrGq9jagcfeSqoD188L_55nkk0IhaFSrw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvOTk5/NDQ1MzIyL3Bob3Rv/L25hbmR1LXJoZWEt/YW1lcmljYW5hLWdy/ZWF0ZXItcmhlYS5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/NGc2MDRpUFBPbDBH/LXlzalBKalVnTmdx/bnFvMGl4SFFGWGhK/dnRhMWRFWT0" alt="Icono" /> </button>
+        </div>
 
 
     );
